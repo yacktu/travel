ce2c917d540c1324820feae6137fa84355f8d7c8
jaxmith <jack.smith@stonybrook.edu>
2018-04-29 14:36:09 -0400

    added support for amazon server plus turorial file

--- travel.py
+++ travel.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
 app = Flask(__name__)
+mysql = MySQL()
+
+#configs
+app.config['MYSQL_DATABASE_USER'] = 'jack'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jacksquared'
+app.config['MYSQL_DATABASE_DB'] = 'travel_agency'
+app.config['MYSQL_DATABASE_HOST'] = 'travelagency.cgydjwsxgwz6.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+mysql.init_app(app)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html')
+
+@app.route('/signUp', methods=['POST', 'GET'])
+def signUp():
+    _flight_carrier = request.form['inputName']
+    _class = request.form['inputEmail']
+    _price = request.form['inputPassword']
+
+    if _flight_carrier and _class and _price:
+
+        conn = mysql.get_db()
+        cursor = conn.cursor()
+        cursor.callproc('new_procedure', (_flight_carrier,_class,_price))
+        data = cursor.fetchall()
+     
+        if len(data) is 0:
+            conn.commit()
+            return json.dumps({'message':'Flight created successfully !'})
+        else:
+            return json.dumps({'error':str(data[0])})
+        
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 if __name__ == "__main__":
     app.run()
